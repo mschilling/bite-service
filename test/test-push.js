f9a69e2611c6231e575da4c6f6e35a494f17b4c7
Michael Schilling <mschilling@arkid.nl>
2016-12-20 08:23:07 +0100

    Add fcm-helper lib

--- test/test-push.js
+++ test/test-push.js
@@ -1,23 +1,22 @@
-var PushService = require('./../push-service');
-
 var fbAuthKey = require('./../.config/config.json').fbAuthKey;
+var fcm = require('../lib/fcm-helper');
+fcm.setAuthorization(fbAuthKey);
 
 var firebase = require('firebase').initializeApp({
-    serviceAccount: "./.config/service-account.json",
-    databaseURL: "https://bite-4ce99.firebaseio.com"
+  serviceAccount: './.config/service-account.json',
+  databaseURL: 'https://bite-4ce99.firebaseio.com'
 });
 
 
+var pl = {
+  to: "fXhOvktGn10:APA91bGIIpPRIBLr8qVopQLPoeSWtFngKTCtv3mMo1eJjdN0qKs1T5BVB09xvpH2MmczBBOTEYkgDaa5dCan2yEnhbibA5kIh1o-Ssjd5o0GvDD7MijHtTpD56Zx0URwViW4RrQRWcG7",
+  data: {
+    type: 0,
+    message: "Hier het bericht 2",
+    title: "Titel van push",
+    bite: "bite key",
+    image_url: 'https://static.thuisbezorgd.nl/images/restaurants/nl/NP07RON/logo_small.png' //
+  }
+}
 
- var pl = {
-        to: "fKxWyn0o4nY:APA91bEvlfda3qkP593bjrNMnlsPuHkAYo8Z-9PuqHyvFN3aPiUsEhPfH8nwYhtFo7qQ8qUiqLWqzit-4nUIl6AluYFSu9l3GucXWYR1pyK94TrcJmrQQo9NRmWilnaQp3XH8fvZJ3Fe",
-        data: {
-            type: 0,
-            message: "Hier het bericht 2",
-            title: "Titel van push",
-            bite: "bite key",
-            image_url: 'https://static.thuisbezorgd.nl/images/restaurants/nl/NP07RON/logo_small.png' //
-        }
-    }
-
-PushService.sendPush(pl);
+fcm.sendPush(pl);
