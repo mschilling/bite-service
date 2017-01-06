7dc1016b20ee42c22fd56d94b6abf22a2549d06a
Michael Schilling <mschilling@arkid.nl>
2017-01-06 14:01:32 +0100

    Merge background-service in main service.js

--- service.js
+++ service.js
@@ -1,3 +1,4 @@
+const moment = require('moment');
 const api = require('./lib/bite-api');
 const ref = api.getFirebaseRef();
 
@@ -11,6 +12,12 @@ ref.child('orders').on('child_added', (snapshot) => {
 });
 
 ref.child('subscribe_queue').on('child_added', onSubscribe);
+
+// On Every Order Added
+ref.child('orders').on('child_added', verifyOrderConsistency);
+
+// On Every Order Removed
+ref.child('orders').on('child_removed', cleanupOrderData);
 
 function onOrderStatusChanged(snapshot) {
   const orderId = snapshot.ref.parent.key;
@@ -125,3 +132,80 @@ function onSubscribe(snapshot) {
   // Cleanup form queue
   setTimeout(() => api.removeSubscriptionFromQueue(snapshot.key), 1000);
 };
+
+// Code below taken from background-service.js
+function verifyOrderConsistency(snapshot) {
+  // let { user, token, topic, subscribe = false } = snapshot.val();
+  const order = snapshot.val();
+  const now = moment.now();
+  let objectChanged = false;
+
+  // order.min_before_closing = order.min_before_closing || defaultOpenTimeInMinutes;
+
+  // let openTime = moment(order.open_time);
+  // let closingTime = moment(openTime).add(defaultOpenTimeInMinutes, 'minutes');
+
+  // let duration = moment.duration(order.min_before_closing, 'minutes');
+  // console.log(duration.humanize());
+
+  if (!order.open_time) {
+    order.open_time = now.valueOf();
+    objectChanged = true;
+  }
+
+  if (!order.status) {
+    order.status = 'new';
+    objectChanged = true;
+  }
+
+  if (objectChanged) {
+    snapshot.ref.set(order);
+  }
+}
+
+function cleanupOrderData(snapshot) {
+  const userOrderRef = ref.child('user_order').child(snapshot.key);
+  const userOrderLockedRef = ref.child('user_order_locked').child(snapshot.key);
+
+  if (snapshot.val().status !== 'closed') {
+    // remove user orders + locked references
+    userOrderRef.remove();
+    userOrderLockedRef.remove();
+  }
+}
+
+function archiveOrders() {
+  console.log(moment().format(), 'Archiving orders');
+  ref.child('orders').once('value').then((snapshot) => {
+    snapshot.forEach(function (orderSnapshot) {
+      let order = orderSnapshot.val();
+      if (order.status === 'closed') {
+        api.archiveOrder(orderSnapshot.key);
+      }
+    });
+  });
+}
+
+function startArchiver() {
+  console.log('Archiver started');
+  let timer = setTimeout(onArchiverTimer, getNextTimerValue());
+
+  function onArchiverTimer() {
+    archiveOrders();
+
+    // Reschedule
+    timer = setTimeout(onArchiverTimer, getNextTimerValue());
+  }
+
+  function getNextTimerValue() {
+    const every5minutes = (moment().add(5, 'minutes'));
+    const hourly = moment().add(1, 'hour').startOf('hour');
+    const midnight = (moment().endOf('day'));
+    // return hourly.diff(moment(), 'milliseconds');
+    return 5000;
+  }
+}
+
+startArchiver();
+
+
