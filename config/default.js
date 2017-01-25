67e06a7cd852e4f93b382158701a47c15c6b45f1
Michael Schilling <mschilling@arkid.nl>
2017-01-25 20:58:33 +0100

    Add development config file

--- config/default.js
+++ config/default.js
@@ -3,12 +3,12 @@
 // More info: // REF: http://goenning.net/2016/05/13/how-i-manage-application-configuration-with-nodejs/
 module.exports = {
   firebase: {
-    databaseURL: '<firebase databaseUrl>',
-    serviceAccount: '<path to file>'
+    databaseURL: process.env.FIREBASE_DB_URL,
+    serviceAccount: process.env.FIREBASE_SERVICE_ACCOUNT
   },
   fcm: {
-    authKey: '<fcm auth key>',
-    debugToken: '<fcm debug token>'
+    authKey: process.env.FCM_AUTH_KEY,
+    debugToken: process.env.FCM_TEST_TOKEN
   },
   log: 'debug'
 };
---
+++ config/development.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = {
+  // Override stuff
+  // firebase: {
+  // }
+};
